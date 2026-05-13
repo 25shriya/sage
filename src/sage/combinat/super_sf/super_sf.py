9465bd2c76c27c6775b23e852a60ca3d1e8ac7f2
Shriya Meenakshisundaram <25shriya@gmail.com>
2026-05-13 14:44:53 +0530

    Added initial super_sf files to /sage/combinat

---
+++ src/sage/combinat/super_sf/super_sf.py
@@ -0,0 +1,37 @@
+"""
+Supersymmetric functions, with their realizations
+
+AUTHORS:
+
+- Shriya M
+"""
+# ****************************************************************************
+#       Copyright (C) 2026 Shriya M <25shriya at gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#                  https://www.gnu.org/licenses/
+# ****************************************************************************
+from sage.structure.parent import Parent
+from sage.structure.unique_representation import UniqueRepresentation
+from sage.categories.unique_factorization_domains import UniqueFactorizationDomains
+from sage.categories.graded_hopf_algebras import GradedHopfAlgebras
+from sage.categories.principal_ideal_domains import PrincipalIdealDomains
+
+class SuperSymmetricFunctions(Parent, UniqueRepresentation):
+    def __init__(self, R):
+        r"""
+        Initialize ``self``.
+
+        EXAMPLES::
+
+            sage: ch = matroids.Wheel(3).chow_ring(QQ, False, 'fy')
+            sage: TestSuite(ch).run()
+        """
+        self._base_ring = R
+        cat = GradedHopfAlgebras(R).Commutative().Cocommutative()
+        if R in PrincipalIdealDomains():
+            cat &= UniqueFactorizationDomains()
+        Parent.__init__(self, category=cat.WithRealizations())
