f9d46f1ff1bfc9e2086581192b0a5fb4b9c7f5b6
Shriya Meenakshisundaram <25shriya@gmail.com>
2026-05-16 17:32:54 +0530

    Added methods to the multiplicative basis class

--- src/sage/combinat/super_sf/super_sfa.py
+++ src/sage/combinat/super_sf/super_sfa.py
@@ -3,12 +3,16 @@ Bases of Supersymmetric functions
 """
 from sage.categories.realizations import Category_realization_of_parent
 from sage.combinat.free_module import CombinatorialFreeModule
+from sage.combinat.partition import Partition
 
 class SuperSymmetricFunctionsBases(Category_realization_of_parent):
     r"""
     The category of bases of supersymmetric functions.
-    sage: SuperSymmetricFunctionsBases()    
-    Category of bases of supersymmetric functions
+
+    EXAMPLES::
+
+        sage: SuperSymmetricFunctionsBases()    
+        Category of bases of supersymmetric functions
     """
     def _repr_(self):
         r"""
@@ -28,9 +32,23 @@ class SuperSymAlgebra_multiplicative(SuperSymAlgebra_generic):
     def __init__(self, base):
         CombinatorialFreeModule.__init__(self, base, base().index_set(), prefix="s", category=SuperSymmetricFunctionsBases(base))
 
+    def product_on_basis(self, left, right):
+        m = list(left) + list(right)
+        m.sort(reverse=True)
+        return self.monomial(Partition(m))
+
+    def coproduct_on_basis(self, mu):
+        T = self.tensor_square()
+        return T.prod(self.coproduct_on_generators(p) for p in mu)
+
     def _repr_(self):
         return "Multiplicative basis of %s" % self.base()
     
     class Element(SuperSymAlgebra_generic.Element):
+        def __init__(self, x_gens, y_gens, alpha_gens):
+            self.x_gens = x_gens
+            self.y_gens = y_gens
+            self.alpha_gens = alpha_gens
+
         def __repr__(self):
             return "%s(%s)" % (self.parent().name(), self)
