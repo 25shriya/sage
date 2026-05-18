8010bbb8b2ac2f5b0622a3c33cd9c82ea8e89bc0
Shriya Meenakshisundaram <25shriya@gmail.com>
2026-05-18 18:21:51 +0530

    Added expand() method to powersum basis

--- src/sage/combinat/super_sf/powersum.py
+++ src/sage/combinat/super_sf/powersum.py
@@ -1,16 +1,25 @@
 from . import super_sfa
 from sage.rings.polynomial.polynomial_ring_constructor import PolynomialRing
+from sage.combinat.partition import Partition
 
 class SupersymFunctionAlgebra_powersum(super_sfa.SuperSymAlgebra_multiplicative):
     def __init__(self, base_ring):
         super.__init__(self, base_ring)
 
     class Element(super_sfa.SuperSymAlgebra_multiplicative.Element):
-        def expand(self, i):
-            variables = self.alpha_gens + self.x_gens + self.y_gens
-            R = PolynomialRing(self.base_ring, variables)
-            l = min(len(self.x_gens), len(self.y_gens))
-            req_sum = R.zero()
-            for j in range(l):
-                req_sum += self.x_gens[j] ** i - self.y_gens[j] ** i
-            return req_sum
+        def expand(self, part=[], alphabet_x='x', alphabet_y='y'):
+            part = Partition(part.sort(reverse=True))
+            prod = self.base_ring.one()
+            for p in part:
+                x_gens = [alphabet_x+str(i).format(i) for i in range(1, p+1)]
+                y_gens = [alphabet_y+str(i).format(i) for i in range(1, p+1)]
+                variables = x_gens + y_gens
+                R = PolynomialRing(self.base_ring, variables)
+                l = min(len(self.x_gens), len(self.y_gens))
+                req_sum = R.zero()
+                for j in range(l):
+                    req_sum += self.x_gens[j] ** p - self.y_gens[j] ** p
+                prod *= req_sum
+            return prod
+        
+        # write embedding function - It's upper triangular
