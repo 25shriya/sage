6a3be99cc5de81f125201c5e8c5287aeaf8b1feb
Shriya Meenakshisundaram <25shriya@gmail.com>
2026-05-22 16:28:20 +0530

    Added super_categories() method

--- src/sage/combinat/super_sf/super_sfa.py
+++ src/sage/combinat/super_sf/super_sfa.py
@@ -3,6 +3,9 @@ Bases of Supersymmetric functions
 """
 from sage.categories.realizations import Category_realization_of_parent
 from sage.combinat.free_module import CombinatorialFreeModule
+from sage.categories.hopf_algebras import HopfAlgebras
+from sage.categories.unique_factorization_domains import UniqueFactorizationDomains
+from sage.categories.principal_ideal_domains import PrincipalIdealDomains
 
 class SuperSymmetricFunctionsBases(Category_realization_of_parent):
     r"""
@@ -19,15 +22,25 @@ class SuperSymmetricFunctionsBases(Category_realization_of_parent):
         """
         return "Category of bases of %s" % self.base()
 
+    def super_categories(self):
+        R = self.base().base_ring()
+        cat = HopfAlgebras(R)
+        categories = [self.base().Realizations(),
+                      cat.Commutative().WithBasis(),
+                      cat.Graded().Realizations()]
+        if R in PrincipalIdealDomains:
+            categories.append(UniqueFactorizationDomains())
+        return categories
+
 class SuperSymAlgebra_generic(CombinatorialFreeModule):
     def __init__(self, base):
         CombinatorialFreeModule.__init__(self, base, category=SuperSymmetricFunctionsBases(base))
 
     class Element(CombinatorialFreeModule.Element):
-            """
-                Generic bases of supersymmetric functions.
-            """
-            pass
+        """
+            Generic bases of supersymmetric functions.
+        """
+        pass
 
 class SuperSymAlgebra_multiplicative(SuperSymAlgebra_generic):
     def __init__(self, base):
