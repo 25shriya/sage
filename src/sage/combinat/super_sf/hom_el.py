7a40605bd5a03d1edc627b729c2b3f6e4ef7cc08
Shriya Meenakshisundaram <25shriya@gmail.com>
2026-06-02 22:35:18 +0530

    Corrected typo in coproduct_on_basis() method

--- src/sage/combinat/super_sf/hom_el.py
+++ src/sage/combinat/super_sf/hom_el.py
@@ -47,7 +47,7 @@ class SupersymFunctionAlgebra_hom_el(super_sfa.SuperSymAlgebra_multiplicative):
 
         def coproduct_on_basis(self, n):
             T = self.tensor_square()
-            return T.sum_of_monomials([Partition([i]), Partition([n-i]) for i in range(1, n+1)])
+            return T.sum_of_monomials((Partition([i]), Partition([n-i])) for i in range(1, n+1))
 
 # What are super_categories? Figure out the math
 # Current problems - creating an element seems impossible. How does the base ring get set? How can you define elements for each partition?
