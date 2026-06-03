44f251c30977978eecd57cd0eed5575540ee2ac5
Shriya Meenakshisundaram <25shriya@gmail.com>
2026-06-03 21:27:00 +0530

    Corrected typos in powersum methods

--- src/sage/combinat/super_sf/powersum.py
+++ src/sage/combinat/super_sf/powersum.py
@@ -11,7 +11,15 @@ from sage.combinat.partition import Partition
 
 class SupersymFunctionAlgebra_powersum(super_sfa.SuperSymAlgebra_multiplicative):
     def __init__(self, Supersym):
-        super_sfa.SuperSymAlgebra_generic(self, SuperSym=Supersym, graded=False)
+        super_sfa.SuperSymAlgebra_generic.__init__(self, SuperSym=Supersym, graded=False)
+
+    def coproduct_on_generators(self, n):
+            part = Partition([n])
+            return tensor([self[part], self[part]])
+
+    def antipode_on_basis(self, n):
+            part = Partition([n])
+            return -self[part]
 
     class Element(super_sfa.SuperSymAlgebra_multiplicative.Element):
         def expand(self, part=[], alphabet_x='x', alphabet_y='y'):
@@ -28,14 +36,6 @@ class SupersymFunctionAlgebra_powersum(super_sfa.SuperSymAlgebra_multiplicative)
                     req_sum += self.x_gens[j] ** p - self.y_gens[j] ** p
                 prod *= req_sum
             return prod
-
-        def coproduct_on_basis(self, n):
-            part = Partition([n])
-            return tensor([self[part], self[part]])
-
-        def antipode_on_basis(self, n):
-            part = Partition([n])
-            return -self[part]
 
         def plethysm(self, i):
             T = self.tensor_square()
