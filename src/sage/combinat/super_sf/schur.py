77bfa808c4999158334918e85351e15336a1eafb
Shriya Meenakshisundaram <25shriya@gmail.com>
2026-08-23 10:57:53 +0530

    Created file for Schur basis

---
+++ src/sage/combinat/super_sf/schur.py
@@ -0,0 +1,13 @@
+r"""
+Schur Basis of Supersymmetric Functions
+
+AUTHORS:
+
+- Shriya M
+"""
+from . import super_sfa
+from sage.rings.polynomial.polynomial_ring_constructor import PolynomialRing
+from sage.combinat.partition import _Partitions, Partition
+from sage.categories.tensor import tensor
+from sage.combinat.sf.sf import SymmetricFunctions
+from sage.misc.misc_c import prod
