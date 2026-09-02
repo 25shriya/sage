039a4978e49b0ee212ff8b60f94da202e31b5fed
Shriya Meenakshisundaram <25shriya@gmail.com>
2026-09-02 11:05:28 +0530

    Added todo

--- src/sage/combinat/super_sf/hom_el.py
+++ src/sage/combinat/super_sf/hom_el.py
@@ -234,3 +234,5 @@ class SupersymFunctionAlgebra_hom_el(super_sfa.SuperSymAlgebra_multiplicative):
                     res *= req_sum
                 fin_res += monomial_coeff[k] * res
             return fin_res
+
+# github comments, return h(e.antipode(f))
